dd45b19075279e1c3330f040904955c717e8e381
Phillip Sturgeon <sturgeonphillip@gmail.com>
2021-05-21 18:39:19 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/build/',
+    port: 8080,
+    proxy: {
+      '/db': 'http://localhost:3000',
+    }
+  }
+}
